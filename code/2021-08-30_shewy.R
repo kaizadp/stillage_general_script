ebd9625833cdde8e42e13cd87d5b7c94c877e5ff
kaizadp <50244730+kaizadp@users.noreply.github.com>
2021-09-12 13:07:42 -0700

    processing script for shewy_2021-08-30 and geobacter_2021-09-01

---
+++ code/2021-08-30_shewy.R
@@ -0,0 +1,45 @@
+## SCRIPT FOR PROCESSING STILLAGE DATA 
+## KFP 2021-09-12
+
+####################### #
+####################### #
+
+
+# I. LOAD PACKAGES -------------------------------------------------------------
+library(tidyverse)  
+library(lubridate)
+library(readxl)
+
+#
+# II. SOURCE THE FUNCTIONS -----------------------------------------------------
+source("code/00-stillage_functions.R")
+
+#
+# III. LOAD FILES --------------------------------------------------------------
+# well plate layout, in wide-form
+dat_layout = read_excel("data/2021-08-30_shewy_layout.xlsx", na = "")
+
+# intensities data, in  wide-form
+dat_data = read.csv("data/2021-08-30_shewy_data.csv", na = "")
+
+#
+# IV. PROCESS DATA -------------------------------------------------------------
+# process the well plate layout data
+layout_processed = process_wellplate_layout(dat_layout)
+
+# process the intensities data and merge with the layout
+data_processed = process_data(dat_data, dat_layout)
+
+#
+# V. PLOT ----------------------------------------------------------------------
+data_processed %>% 
+  ggplot(aes(x = HOURS, 
+             y = intensity_bl_corrected, 
+             color = as.character(Wavelength)))+
+  geom_line()+
+  facet_wrap(~sample_name, scales = "free_y")+
+  labs(color = "wavelength, nm",
+       title = "Shewanella, 2021-08-30")+
+  theme_bw()
+
+ggsave("output/2021-08-30_shewy.png")
